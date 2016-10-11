7521188d50e61c4db0435bad55486ea6eaf13fb7
Aenadon <aenadon@outlook.com>
2016-10-11 08:08:53 +0200

    Name chooser fix

--- public/chatscript.js
+++ public/chatscript.js
@@ -4,32 +4,41 @@ document.addEventListener("DOMContentLoaded", function() {
     var modalContainer = document.getElementById("modalContainer");
     var modalInput = document.getElementById("usernameInput");
 
+    var socket;
+
+    var receiveMessageContainer = document.getElementById("receiveMessageContainer");
+    var inputBox = document.getElementById("inputBox");
+    var sendButton = document.getElementById("sendButton");
+
     modalInput.addEventListener("keypress", function(key) { // check for enter/shift+enter
         if (key.keyCode === 13) {
             console.log(modalInput);
             username = modalInput.value;
             modalContainer.style.display = "none";
+
+            socket = io();
+            socket.emit("chatMessage", {username: username, message: " is now online!", isMetaMessage: true, isOnline: true});
+            inputBox.focus();
+
+            socket.addEventListener("loadMessages", function(messageList) {
+                for (var i = 0; i < messageList.length; i++) {
+                    displayText(messageList[i]);
+                }
+            });
+
+            socket.addEventListener("receiveMessage", function(message) {
+                displayText(message);
+                notify(message.username + ": " + message.message);
+            });
         }
     });
-    
+
     if (!Notification) {
         alert('Desktop notifications not available in your browser. Try Chromium.');
     } else if (Notification.permission !== "granted") {
         Notification.requestPermission();
     }
 
-    var socket = io();
-
-    do {
-        //username = prompt("What is your name?");
-    } while (!username);
-    socket.emit("chatMessage", {username: username, message: " is now online!", isMetaMessage: true, isOnline: true});
-
-    var receiveMessageContainer = document.getElementById("receiveMessageContainer");
-    var inputBox = document.getElementById("inputBox");
-    var sendButton = document.getElementById("sendButton");
-
-    inputBox.focus(); // put cursor directly into inputBox
     inputBox.addEventListener("keypress", function(key) { // check for enter/shift+enter
         if (key.keyCode === 13 && !key.shiftKey) {
             submitMessage(key); // send message only if enter and not shift is pressed
@@ -38,12 +47,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     sendButton.addEventListener("click", function(event) {
         submitMessage(event); // send message if send button was clicked
-    });
-
-    socket.addEventListener("loadMessages", function(messageList) {
-        for (var i = 0; i < messageList.length; i++) {
-            displayText(messageList[i]);
-        }
     });
 
     window.onbeforeunload = function(event) {
@@ -75,11 +78,6 @@ document.addEventListener("DOMContentLoaded", function() {
             notification.close();
         };
     }
-
-    socket.addEventListener("receiveMessage", function(message) {
-        displayText(message);
-        notify(message.username + ": " + message.message);
-    });
 
     function displayText(message) {
         var receiveBox = document.getElementById("receiveMessageContainer");
